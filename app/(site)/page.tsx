cd31efba247aa062dc294cb5b8e73994bc607725
Emin Kumru <eminkumru135@outlook.com>
2023-08-13 16:27:03 +0300

    supabase setup

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <div
       className="
-  bg-neutral-900
-  rounded-lg
-  h-full
-  w-full
-  overflow-hidden
-  overflow-y-auto
-  "
+      bg-neutral-900
+        rounded-lg
+        h-full
+        w-full
+        overflow-hidden
+        overflow-y-auto
+      "
     >
       <Header>
         <div className="mb-2">
